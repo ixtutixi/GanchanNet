142b864a71b0b7c56caca652777b6e70b95b7911
ixtutixi <ixtutixi0@icloud.com>
2023-04-27 02:36:58 +0900

    Initial Commit

---
+++ GanChan Network/GanChan_NetworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  GanChan_NetworkApp.swift
+//  GanChan Network
+//
+//  Created by 高橋龍一 on 2023/04/27.
+//
+
+import SwiftUI
+
+@main
+struct GanChan_NetworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GanChan NetworkUITests/GanChan_NetworkUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  GanChan_NetworkUITestsLaunchTests.swift
+//  GanChan NetworkUITests
+//
+//  Created by 高橋龍一 on 2023/04/27.
+//
+
+import XCTest
+
+final class GanChan_NetworkUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
